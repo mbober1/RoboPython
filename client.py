bcc21b35d2caa18540ca7c3c049631f30eed993a
Cezary Bober <ceski23@gmail.com>
2020-02-02 15:30:22 +0100

    Fix aborting programm

--- client.py
+++ client.py
@@ -1,30 +1,37 @@
-import pygame, time, threading, cv2
+import pygame
+import time
+import threading
+import cv2
 from clientmods import *
 import asyncio
 from pygame.locals import *
 from ping3 import ping
 
 import numpy as np
-import sys, random, time
+import sys
+import random
+import time
 import logging
 
 
 def FindJoysticks():
     print("Found " + str(pygame.joystick.get_count()) + " devices")
-    if pygame.joystick.get_count()>1: 
+    if pygame.joystick.get_count() > 1:
         for i in range(0, pygame.joystick.get_count()):
             print('[' + str(i) + ']' + pygame.joystick.Joystick(i).get_name())
         device = int(input("Choose one: "))
-    else: device = 0
+    else:
+        device = 0
 
     return device
+
 
 class Control_device():
     axis_data = None
     button_data = None
     power = None
     direction = None
-    
+
     def __init__(self):
         pygame.init()
         pygame.joystick.init()
@@ -34,21 +41,21 @@ class Control_device():
                 self.joy = pygame.joystick.Joystick(FindJoysticks())
                 self.joy.init()
                 print(self.joy.get_name(), ' connected')
-                print(self.joy.get_numaxes(), 'Axes,', self.joy.get_numbuttons(), 'Buttons')
+                print(self.joy.get_numaxes(), 'Axes,',
+                      self.joy.get_numbuttons(), 'Buttons')
 
             except pygame.error as message:
                 self.keyboard = 1
                 print("Keyboard control")
 
-        else: 
+        else:
             self.keyboard = 1
             print("Keyboard control")
 
-    
     def listen(self):
         if not self.axis_data:
             self.axis_data = {}
-            
+
         for event in pygame.event.get():
             print('E:', event)
             if event.type == pygame.key.get_pressed():
@@ -65,10 +72,10 @@ class Control_device():
 
             elif event.type == pygame.JOYAXISMOTION:
                 # print('pad')
-                self.axis_data[event.axis] = round(event.value,2)
+                self.axis_data[event.axis] = round(event.value, 2)
 
         # if not self.keyboard:
-        
+
         #     if not self.axis_data:
         #         self.axis_data = {}
 
@@ -76,7 +83,6 @@ class Control_device():
         #     #     self.button_data = {}
         #     #     for i in range(self.joy.get_numbuttons()):
         #     #         self.button_data[i] = False
-
 
         #     # print(f'C:{pygame.e}')
         #     for event in pygame.event.get():
@@ -91,8 +97,8 @@ class Control_device():
         #             # In the current setup, I have the state simply printing out to the screen.
         #         print(9)
         #         return self.axis_data
-        
-        # if self.keyboard: #TU SKONCZYLEM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1111111  
+
+        # if self.keyboard: #TU SKONCZYLEM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1111111
         #     print("dupa1")
         #     for event in pygame.event.get():
         #         print("dupa")
@@ -109,7 +115,6 @@ class Control_device():
         #                 self.direction = 90
 
 
-
 async def Control():
     connection = Client()
     steering = Control_device()
@@ -117,40 +122,36 @@ async def Control():
         # connection.connect()
         time1 = time.time()
         while True:
-            try:
-                # time.sleep(0.2)
-                await asyncio.sleep(0)
-                # print(steering.listen())
-                # pygame.event.get()
+            # time.sleep(0.2)
+            await asyncio.sleep(0)
+            # print(steering.listen())
+            # pygame.event.get()
 
-                # rt = joy.get_axis(4)
-                # lt = joy.get_axis(5)
-                # x = joy.get_button(1)
-                # l1 = joy.get_button(4)
-                # r1 = joy.get_button(5)
+            # rt = joy.get_axis(4)
+            # lt = joy.get_axis(5)
+            # x = joy.get_button(1)
+            # l1 = joy.get_button(4)
+            # r1 = joy.get_button(5)
 
-                # if left_trigger != lt:
-                #     left_trigger = lt
-                #     connection.send_data('LT: ' + str(round((left_trigger + 1) / 2, 2)))
+            # if left_trigger != lt:
+            #     left_trigger = lt
+            #     connection.send_data('LT: ' + str(round((left_trigger + 1) / 2, 2)))
 
-                # if right_trigger != rt:
-                #     right_trigger = rt
-                #     connection.send_data('RT: ' + str(round((right_trigger + 1) / 2, 2)))
+            # if right_trigger != rt:
+            #     right_trigger = rt
+            #     connection.send_data('RT: ' + str(round((right_trigger + 1) / 2, 2)))
 
-                # if x:
-                #     print('Abort!')
-                #     exit()
+            # if x:
+            #     print('Abort!')
+            #     exit()
 
-                # if l1!=l1_old:
-                #     l1_old=l1
-                #     connection.send_data('l1: ' + str(l1))
+            # if l1!=l1_old:
+            #     l1_old=l1
+            #     connection.send_data('l1: ' + str(l1))
 
-                # if r1 != r1_old:
-                #     r1_old = r1
-                #     connection.send_data('r1: ' + str(r1))
-            except KeyboardInterrupt:
-                print('Control ends')
-                break
+            # if r1 != r1_old:
+            #     r1_old = r1
+            #     connection.send_data('r1: ' + str(r1))
 
     except ConnectionRefusedError:
         print("Server refused the connection")
@@ -160,83 +161,72 @@ async def Control():
         print("Server closed connection")
 
 
-        
-
-
-
-
-
-
-
-
 async def Player():
-    
+
     window = Window()
 
     timer = time.time()
     while True:
         await asyncio.sleep(0)
-        try:
-            fps_timer = time.time()
-            if(time.time()- timer > 1):
-                window.Render_ping()
-                timer = time.time()
+        fps_timer = time.time()
+        if(time.time() - timer > 1):
+            window.Render_ping()
+            timer = time.time()
 
-            window.Play()
-            pygame.display.update()
+        window.Play()
+        pygame.display.update()
 
-            if (time.time()-fps_timer) < (1/constant.FPS):
-                time.sleep((1/constant.FPS)-(time.time()-fps_timer))
+        if (time.time()-fps_timer) < (1/constant.FPS):
+            time.sleep((1/constant.FPS)-(time.time()-fps_timer))
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit(2)
-            
-        except KeyboardInterrupt:
-            cv2.destroyAllWindows()
-            print('Preview ends')
-            break
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(2)
 
 
 class Window():
-    white = (255, 255, 255) 
+    white = (255, 255, 255)
     black = (0, 0, 0)
 
     def __init__(self):
         pygame.font.init()
         pygame.display.set_caption("RaspiRobot Camera")
-        self.screen = pygame.display.set_mode([640,480])
-        self.screen.fill([0,0,0])
+        self.screen = pygame.display.set_mode([640, 480])
+        self.screen.fill([0, 0, 0])
         self.font = pygame.font.Font('freesansbold.ttf', 11)
         self.fps = 0
         self.Render_ping()
         self.camera = cv2.VideoCapture(constant.LINK)
-    
+
     def Render_ping(self):
         temp_ping = ping(constant.HOST, unit='ms', timeout=1)
-        self.ping_plane = self.font.render('Ping: ' + str(int(temp_ping)) + 'ms', True, self.white, self.black)
-        self.fps_plane = self.font.render('Fps: ' + str(self.fps), True, self.white, self.black)
-        self.fps=0
+        self.ping_plane = self.font.render(
+            'Ping: ' + str(int(temp_ping)) + 'ms', True, self.white, self.black)
+        self.fps_plane = self.font.render(
+            'Fps: ' + str(self.fps), True, self.white, self.black)
+        self.fps = 0
 
     def Play(self):
         self.ret, self.frame = self.camera.read()
         self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
-        self.frame = self.frame.swapaxes(0,1)
+        self.frame = self.frame.swapaxes(0, 1)
         self.frame = pygame.surfarray.make_surface(self.frame)
-        self.screen.blit(self.frame, (0,0))
+        self.screen.blit(self.frame, (0, 0))
         self.fps = self.fps+1
 
-        self.screen.blit(self.ping_plane, (0,0))
-        self.screen.blit(self.fps_plane, (100,0))
-        
+        self.screen.blit(self.ping_plane, (0, 0))
+        self.screen.blit(self.fps_plane, (100, 0))
 
-async def main():
+
+def main():
+    tasks = None
+    loop = asyncio.get_event_loop()
+    pygame.init()
     try:
-        pygame.init()
-        ft = asyncio.gather(Player(), Control())
-        await asyncio.sleep(5)
-        ft.cancel()
-    except asyncio.exceptions.CancelledError:
-        print('dupaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+        tasks = asyncio.gather(Player(), Control())
+        loop.run_until_complete(tasks)
+    except KeyboardInterrupt:
+        print('Ending program')
 
-asyncio.run(main())
+
+main()
